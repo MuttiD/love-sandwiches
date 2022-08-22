435487c239976268e3c9decf7d4f2eaa442afc40
MuttiD <dgmutti@hotmail.com>
2022-08-22 17:27:45 +0000

    Add slaes data to sales worksheet google sheet

--- run.py
+++ run.py
@@ -20,7 +20,10 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
     """ 
-    Get sales figures input from the user 
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid. 
     """
     # adding a while loop so that we are not needing to run the programm manually all the time
     # and it will stop once the required parametres are satisfied (6 integers numbers)
@@ -63,4 +66,19 @@ def validate_data(values):
     return True    
 
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    
+    # getting into the sales tab in the worksheet
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
